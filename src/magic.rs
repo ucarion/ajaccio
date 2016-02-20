e0b8ad0491ac8c68073c90e728d06de564a41490
Ulysse Carion <ulysse@ulysse.io>
2016-02-19 22:55:10 -0800

    Add bishop_attacks

--- src/magic.rs
+++ src/magic.rs
@@ -23,6 +23,35 @@ pub fn rook_attacks(square: Square) -> Bitboard {
     result
 }
 
+fn is_in_bounds(file: i8, rank: i8) -> bool {
+    1 <= file && file <= 6 && 1 <= rank && rank <= 6
+}
+
+pub fn bishop_attacks(square: Square) -> Bitboard {
+    let start = (square.file() as i8, square.rank() as i8);
+
+    diagonal_attacks(start, 1, 1) |
+        diagonal_attacks(start, 1, -1) |
+        diagonal_attacks(start, -1, 1) |
+        diagonal_attacks(start, -1, -1)
+}
+
+fn diagonal_attacks(start: (i8, i8), dx: i8, dy: i8) -> Bitboard {
+    let mut result = Bitboard::new(0);
+    let mut cursor = start;
+    loop {
+        cursor = (cursor.0 + dx, cursor.1 + dy);
+
+        if is_in_bounds(cursor.0, cursor.1) {
+            result = result | Square::from_coords(cursor.0 as u8, cursor.1 as u8).to_bitboard();
+        } else {
+            break;
+        }
+    }
+
+    result
+}
+
 #[test]
 fn test_rook_attacks() {
     // TODO: Is there a better way to establish that I want these cases to work than to hard-code
@@ -34,4 +63,15 @@ fn test_rook_attacks() {
     assert_eq!(a1, rook_attacks(Square::from_san("a1")));
     assert_eq!(e4, rook_attacks(Square::from_san("e4")));
     assert_eq!(h8, rook_attacks(Square::from_san("h8")));
+}
+
+#[test]
+fn test_bishop_attacks() {
+    let a1 = Bitboard::new(18049651735527936);
+    let e4 = Bitboard::new(637888545440768);
+    let h8 = Bitboard::new(18049651735527936);
+
+    assert_eq!(a1, bishop_attacks(Square::from_san("a1")));
+    assert_eq!(e4, bishop_attacks(Square::from_san("e4")));
+    assert_eq!(h8, bishop_attacks(Square::from_san("h8")));
 }
